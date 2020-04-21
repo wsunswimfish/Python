8f3dde284851c95348d318831e581a73c8b9cbf0
zhoujianpeng <zhouziyang@126.com>
2020-04-21 09:29:21 +0800

    OLED 显示 DEMO

--- OLED/schedule_time.py
+++ OLED/schedule_time.py
@@ -5,12 +5,7 @@ import schedule
 from printc import printc
 
 
-def sch_coffeebreak(text="茶歇时间，请休息一会，站起来活动一下。"):
-    printc(text, "black", "cyan")
-    # pyttsx3.speak(text)
-
-
-def job(text="消息提示！"):
+def job(text="消息提示！",front="black",back="cyan"):
     printc(text + "  {}".format(time.strftime("%H:%M:%S", time.localtime())), "black", "cyan")
     pyttsx3.speak(text)
 
@@ -21,16 +16,16 @@ def main():
                    "clock": ['23:00', '01:00', '03:00', '05:00', '07:00', '09:00', '11:00', '13:00', '15:00', '17:00',
                              '19:00', '21:00'],
                    "gengdian": ["三更", "四更", "五更", "", "", "", "", "", "", "", "一更", "二更"],
-                   "say":["","","","","","","","","","","",""]}
-    shichen = "子丑寅卯辰巳午未申酉戌亥"
-    shichen_time = ["{:0>2}:00".format(i * 2 - 1) if i != 0 else "23:00" for i in range(12)]
+                   "say": ["", "", "", "", "", "", "", "", "", "", "", ""]}
     try:
         for i in range(12):
             schedule.every().day.at(shichen_dic["clock"][i]).do(job, (
-                "{} {} {}".format(shichen_dic["name"][i], shichen_dic["name1"][i], shichen_dic["gengdian"][i])))
+                "{},{},{}".format(shichen_dic["name"][i], shichen_dic["name1"][i], shichen_dic["gengdian"][i])))
 
         schedule.every().day.at("08:30").do(job, "一天的工作时间开始了！")
         schedule.every().day.at("10:00").do(job, "站起来活动活动，休息一下！")
+
+
         schedule.every().day.at("12:00").do(job, "午餐时间到！")
         schedule.every().day.at("12:30").do(job, "中午休息一会吧！")
         schedule.every().day.at("13:30").do(job, "下午工作时间开始了！")
@@ -40,10 +35,15 @@ def main():
             schedule.run_pending()
             time.sleep(1)
     except:
-        printc("程序异常终止!", "red", "white")
+        job("程序异常终止!", "red", "white")
     finally:
         schedule.clear()
-
+        printc("程序执行结束。","red","white")
+        pyttsx3.speak("程序执行结束。")
 
 if __name__ == "__main__":
+    engine=pyttsx3.init()
+    engine.setProperty("rate",150)
+    engine.setProperty("voice",'com.apple.speech.synthesis.voice.mei-jia.premium')
+    job("报时程序开始执行")
     main()
